dabb4467e8d558ce9fd58adf4abd369479291898
tytjan <adrian-czech@outlook.com>
2023-02-26 22:25:32 +0100

    coś tam

---
+++ compress.c
@@ -0,0 +1,114 @@
+#include "compress.h"
+#include "Node_vector.h"
+#include <stdlib.h>
+#include <string.h>
+
+void traverse(Tree_node* node, Bit_vector* bit_vector, Bit_vector** codewords)
+{
+    if (node->left)
+    {
+        push_bit(bit_vector, '0');
+        traverse(node->left, bit_vector, codewords);
+        pop_bit(bit_vector);
+    }
+
+    if (node->right)
+    {
+        push_bit(bit_vector, '1');
+        traverse(node->right, bit_vector, codewords);
+        pop_bit(bit_vector);
+    }
+
+    if (node->has_value)
+    {
+        codewords[node->value] = copy_bit_vector(bit_vector);
+    }
+}
+
+static void print_bit_vector(const Bit_vector* vector, char* str)
+{
+    size_t i;
+    for (i = 0; i < vector->size; i++)
+    {
+        strcat(str, vector->bits[i] == '0' ? "0" : "1"); // it needs a string
+    }
+    strcat(str, "\n");
+}
+
+char* get_codewords(FILE* input)
+{
+    const size_t header_size = 256 * 8;
+    char* header = calloc(header_size, 1);
+
+    Node_vector node_vector = init_node_vector();
+
+    int byte;
+    while ((byte = fgetc(input)) != EOF)
+    {
+        Tree_node* node = get_node_of_value(&node_vector, byte);
+        node->frequency++;
+    }
+    fclose(input);
+
+    remove_zero_nodes(&node_vector);
+
+    while (node_vector.size > 1)
+    {
+
+        Tree_node* left = node_vector.nodes[0];
+        Tree_node* right = node_vector.nodes[1];
+
+        Tree_node* new_node = merge_nodes(left, right);
+
+        pop_2_front_nodes(&node_vector);
+        push_back_node(&node_vector, new_node);
+
+        sort_vector(&node_vector);
+    }
+    Tree_node* huffman_tree = node_vector.nodes[0];
+
+    pop_front_node(&node_vector);
+    free(node_vector.nodes);
+
+
+    if (!huffman_tree->left) // only one character
+    {
+        char* result = malloc(5);
+        sprintf(result, "%02X0\n", huffman_tree->value);
+        free_tree(huffman_tree);
+        return result;
+    }
+
+    Bit_vector* codewords[256];
+    int i;
+    for (i = 0; i < 256; i++)
+        codewords[i] = NULL;
+
+    Bit_vector bit_vector = init_bit_vector();
+
+    traverse(huffman_tree, &bit_vector, codewords);
+
+    for (i = 0; i < 256; i++)
+    {
+        if (codewords[i] != NULL)
+        {
+            char header_line[12] = {0}; // byte value (2 characters), codeword (up to 8 characters), endline, '\0'
+
+            sprintf(header_line, "%02X", i); // 2 nibbles
+            print_bit_vector(codewords[i], header_line + 2); // then codeword, and then newline
+            strcat(header, header_line);
+        }
+    }
+
+    free_bit_vector(&bit_vector);
+
+    for (i = 0; i < 256; i++)
+    {
+        free_bit_vector(codewords[i]);
+        free(codewords[i]);
+    }
+
+    free_tree(huffman_tree);
+
+    return header;
+}
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "Node_vector.h"
-#include "Bit_vector.h"
+#include "compress.h"
+#include <stdlib.h>
 
 void print_nodes(Node_vector node_vector) // DEBUG
 {
@@ -12,40 +12,14 @@ void print_nodes(Node_vector node_vector) // DEBUG
     }
 }
 
-
-void print_bit_vector(const Bit_vector* vector)
-{
-    size_t i;
-    for (i = 0; i < vector->size; i++)
-        printf("%c", vector->bits[i]);
-    printf("\n");
-}
-
-void traverse(Tree_node* node, Bit_vector* bit_vector, Bit_vector** codewords)
-{
-    if (node->left)
-    {
-        push_bit(bit_vector, '0');
-        traverse(node->left, bit_vector, codewords);
-        pop_bit(bit_vector);
-    }
-
-    if (node->right)
-    {
-        push_bit(bit_vector, '1');
-        traverse(node->right, bit_vector, codewords);
-        pop_bit(bit_vector);
-    }
-
-    if (node->has_value)
-    {
-        codewords[node->value] = copy_bit_vector(bit_vector);
-    }
-}
-
-
 int main(int argc, char** argv)
 {
+    if (argc < 3)
+    {
+        fprintf(stderr, "input output\n");
+        return 1;
+    }
+
     FILE* input = fopen(argv[1], "rb");
     if (input == NULL)
     {
@@ -53,64 +27,11 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    Node_vector node_vector = init_node_vector();
+    char* header = get_codewords(input);
 
-    int byte;
-    while ((byte = fgetc(input)) != EOF)
-    {
-        Tree_node* node = get_node_of_value(&node_vector, byte);
-        node->frequency++;
-    }
+    printf("%s\n", header);
+    free(header);
+
     fclose(input);
-
-    remove_zero_nodes(&node_vector);
-
-    while (node_vector.size > 1)
-    {
-
-        Tree_node* left = node_vector.nodes[0];
-        Tree_node* right = node_vector.nodes[1];
-
-        Tree_node* new_node = merge_nodes(left, right);
-
-        pop_2_front_nodes(&node_vector);
-        push_back_node(&node_vector, new_node);
-
-        sort_vector(&node_vector);
-    }
-    Tree_node* huffman_tree = node_vector.nodes[0];
-
-    pop_front_node(&node_vector);
-    free(node_vector.nodes);
-
-
-    Bit_vector* codewords[256];
-    int i;
-    for (i = 0; i < 256; i++)
-        codewords[i] = NULL;
-
-    Bit_vector bit_vector = init_bit_vector();
-
-    traverse(huffman_tree, &bit_vector, codewords);
-
-    for (i = 0; i < 256; i++)
-    {
-        if (codewords[i] != NULL)
-        {
-            printf("%c ", i);
-            print_bit_vector(codewords[i]);
-        }
-    }
-
-    free_bit_vector(&bit_vector);
-
-    for (i = 0; i < 256; i++)
-    {
-        free_bit_vector(codewords[i]);
-        free(codewords[i]);
-    }
-
-    free_tree(huffman_tree);
-
     return 0;
 }
